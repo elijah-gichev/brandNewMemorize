2adc25d6e794a532eeb37e18675516c60839a5a8
elijah-gichev <elijah.gichev@gmail.com>
2022-02-02 10:31:35 +0300

    Initial Commit

---
+++ brandNewMemorize/brandNewMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  brandNewMemorizeApp.swift
+//  brandNewMemorize
+//
+//  Created by Илия Гичев on 02.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct brandNewMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
